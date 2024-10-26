c8ee4d8b4d15029336838e075408072817fbb154
migarakavishan <migarakavishan43@gmail.com>
2024-10-27 00:03:40 +0530

    first commit

---
+++ lib/screen/category_news.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/model/new_model.dart';
+import 'package:newsapp/screen/home_screen.dart';
+import 'package:newsapp/screen/news_details.dart';
+import 'package:newsapp/services/services.dart';
+
+// ignore: must_be_immutable
+class SelectedCategoryNews extends StatefulWidget {
+  String category;
+  SelectedCategoryNews({super.key, required this.category});
+
+  @override
+  State<SelectedCategoryNews> createState() => _SelectedCategoryNewsState();
+}
+
+class _SelectedCategoryNewsState extends State<SelectedCategoryNews> {
+  List<NewsModel> articles = [];
+  bool isLoading = true;
+  getNews() async {
+    CategoryNews news = CategoryNews();
+    await news.getNews(widget.category);
+    articles = news.dataStore;
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    getNews();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        leading: BackButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const NewsHomeScreen(),
+                ));
+          },
+        ),
+        title: Text(
+          widget.category,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : SingleChildScrollView(
+              child: ListView.builder(
+                itemCount: articles.length,
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  final article = articles[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                NewsDetails(newsModel: article),
+                          ));
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(15),
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.network(
+                              article.urlToImage!,
+                              height: 250,
+                              width: 400,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            article.title!,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          const Divider()
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+    );
+  }
+}
---
+++ lib/screen/news_details.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/model/new_model.dart';
+import 'package:newsapp/screen/home_screen.dart';
+
+class NewsDetails extends StatelessWidget {
+  final NewsModel newsModel;
+  const NewsDetails({super.key, required this.newsModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: BackButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const NewsHomeScreen(),
+                  ));
+            },
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              Text(
+                newsModel.title!,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              Row(
+                children: [
+                  const Expanded(child: SizedBox()),
+                  Expanded(
+                      child: Text(
+                    "- ${newsModel.author!}",
+                    maxLines: 1,
+                  ))
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Image.network(newsModel.urlToImage!),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                newsModel.content!,
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                newsModel.description!,
+                style: const TextStyle(fontSize: 18),
+              )
+            ],
+          ),
+        ));
+  }
+}
